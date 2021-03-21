fe91115ef799386cb8edbb77b0e95bf9153ffeb2
lazaroofarrill <lazaroofarrill@gmail.com>
2021-03-21 19:13:02 -0400

    v2.0.1

---
+++ import-..js
@@ -0,0 +1,4 @@
+export const icons = {
+    "menu": "M6 7l5 0a1 1 0 1 1 0 2L6 9a1 1 0 1 1 0 -2zm7 8l5 0a1 1 0 0 1 0 2L13 17a1 1 0 1 1 0 -2zM6 11l12 0a1 1 0 0 1 0 2L6 13a1 1 0 1 1 0 -2z",
+    "node_modules_loader": "null"
+}
